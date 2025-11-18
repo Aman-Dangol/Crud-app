20479d62ecb823bfa7f06f1d5a3cb2eedf091b3c
Aman-Dangol <amandangol423@gmail.com>
2025-11-18 16:53:30 +0545

    feat(update): udpate complete

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@ app.post("/task", async (req, res) => {
   try {
     await primsa.post.create({ data: { title: body.name } });
   } catch (e) {
-    console.log(e);
     res.json({ message: "task couldnt be added" });
   }
   res.json({ message: "tasks added succesfully" });
@@ -39,11 +38,28 @@ app.delete("/deleteTask", async (req, res) => {
         id,
       },
     });
-  } catch {
+  } catch (e) {
+    console.log(e);
     res.json({ message: "delete failed" });
     return;
   }
-  res.json({ message: "delete failed" });
+  res.json({ message: "delete was a success" });
+});
+
+app.put("/updateTask", async (req, res) => {
+  const { id, name } = req.body;
+
+  try {
+    const updated = await primsa.post.update({
+      where: { id },
+      data: { title: name },
+    });
+
+    return res.json({ message: "update success", updated });
+  } catch (e) {
+    console.log(e);
+    return res.status(404).json({ message: "update failed" });
+  }
 });
 
 app.listen(3000);
